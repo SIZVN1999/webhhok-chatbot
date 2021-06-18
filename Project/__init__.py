b78131839599c380bd035cc93d72621f2aeb25c2
SIZVN1999 <nk.charoensuk@gmail.com>
2021-06-18 17:56:02 +0700

    ...

--- Project/__init__.py
+++ Project/__init__.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from flask import Flask, request, abort
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -9,80 +9,87 @@ app = Flask(__name__)
 def hello():
     return 'hello sizvn', 200
 
-@app.route('/webhook', methods = ['POST', 'GET'])
+
+@app.route('/webhook', methods=['POST', 'GET'])
 def webhook():
     req = request.get_json(silent=True, force=True)
     query_result = req.get('queryResult')
     fulfillmentText = ''
     action = query_result.get('action')
     # print(action)
-    
+
     if (action == 'add.number'):
-        
+
         numb1 = int(query_result.get('parameters').get('numb1'))
         numb2 = int(query_result.get('parameters').get('numb2'))
         result = str(numb1+numb2)
         fulfillmentText = str(numb1)+" + "+str(numb2)+" = "+result
-        return {
+        data = {
             "fulfillmentMessages": [
                 {
                     "payload": {
                         "line": {
-                            
+
                             "type": "text",
                             "text": fulfillmentText
                         }
                     }
                 }
             ]
-        }, 200
-        
+        }
+        return jsonify(data), 200
+
     elif(action == 'get.province'):
         url = "http://192.168.3.13:3000/address/province"
         response = requests.request("GET", url)
         data = response.json()
-        
-        fulfillmentMessage = "จังหวัดในประเทศไทยมีทั้งหมด "+str(len(data['payload']))+" จังหวัด ได้แก่ \n"
-        
-        for index, item in enumerate(data['payload']):
-            fulfillmentMessage += "province "+str(index+1)+ " : "+item['NAME_TH']+" \n"
 
-        return {
+        fulfillmentMessage = "จังหวัดในประเทศไทยมีทั้งหมด " + \
+            str(len(data['payload']))+" จังหวัด ได้แก่ \n"
+
+        for index, item in enumerate(data['payload']):
+            fulfillmentMessage += "province " + \
+                str(index+1) + " : "+item['NAME_TH']+" \n"
+
+        data = {
             "fulfillmentMessages": [
                 {
                     "payload": {
                         "line": {
-                            
+
                             "type": "text",
                             "text": fulfillmentMessage
                         }
                     }
                 }
             ]
-        }, 200
-        
+        }
+
+        return jsonify(data), 200
+
     elif (action == 'get.productbyId'):
         numb1 = str(query_result.get('parameters').get('number'))
         numb1 = numb1.split('.')
         numb1 = numb1[0]
-        
+
         url = "http://192.168.3.13:3000/Line/product/"+numb1
         response = requests.request("GET", url)
         data = response.json()
-        
+
         product_name = str(data['Product_Name'])
-        
-        return  {
+
+        data = {
             "fulfillmentMessages": [
                 {
                     "payload": {
                         "line": {
-                            
+
                             "type": "text",
                             "text": "สินค้าที่คุณตามหา คือ : \n"+product_name
                         }
                     }
                 }
             ]
-        },200
-        
+        }
+
+        return jsonify(data), 200
--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import json
 from Project import app
 
 if __name__ == '__main__':
-    app.run(port = 200)
+    app.run(port = 200, debug=False)
     
