d5381c83e145544b7c81d4abf6b8b82c99b38f83
SIZVN1999 <nk.charoensuk@gmail.com>
2021-06-18 18:06:11 +0700

    ...

--- Project/__init__.py
+++ Project/__init__.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    print()
     return 'hello sizvn', 200
 
 
--- app.py
+++ app.py
@@ -3,5 +3,6 @@ import json
 from Project import app
 
 if __name__ == '__main__':
-    app.run(port = 200, debug=False)
+    # app.run(port = 200, debug=False)
+    app.run(debug=False)
     
